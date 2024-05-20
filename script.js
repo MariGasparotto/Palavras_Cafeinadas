d2807a3e4124357bb03b5b15acc578d3814abc86
MariGasparotto <168597975+MariGasparotto@users.noreply.github.com>
2024-05-20 13:37:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let currentSlide = 0;
+
+function updateCarousel(){
+    const carousel = document.querySelector('.carousel');
+    const itemWidth = document.querySelector('.carousel .container-itens');
+    const offset = currentSlide * -itemWidth;
+    carousel.style.transform = `translateX(${offset}px)`;
+}
+
+function nextslide(){
+    const totalItems = document.querySelectorAll('.carousel .container-itens');
+    const visibleItems = 3;
+    if(currentSlide < totalItems - visibleItems) {
+        currentSlide++
+        updateCarousel();
+    }
+}
+
+function prevSlide() {
+    if (currentSlide < 0) {
+        currentSlide--;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    updateCarousel();
+})
+
+$('.carousel').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  });
